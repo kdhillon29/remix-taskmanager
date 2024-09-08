c5658a2d763d7c6de6d8cc46e51b447c5accc1d1
kanwar <kdhillon469@gmail.com>
2024-09-08 11:27:10 +1000

    add protected rouute authentication

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,29 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import { authenticator } from "~/utils/auth.server";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "New Remix taskmanager App" },
+    {
+      name: "description",
+      content:
+        "Simple taskmanager app with remix,prisma,mongodb and zod validation!",
+    },
   ];
 };
-
+//protect the page from unauthenticated users
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+  return { user };
+};
 export default function Index() {
   return (
-    <div className="container bg-slate-600 h-screen mx-auto text-center p-2">
-      <h1 className="bg-red-500 w-1/2 mx-auto">Welcome to Remix</h1>
+    <div className="container bg-purple-200 h-screen mx-auto text-center p-2">
+      <h1 className="bg-blue-200 text-3xl w-[80%] mx-auto p-4 tracking-wide">
+        Welcome to Remix TaskaManager App
+      </h1>
     </div>
   );
 }
