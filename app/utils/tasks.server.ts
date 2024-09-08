85887a6edcc3b08ffe82301ab857a76410acbbf0
kanwar <kdhillon469@gmail.com>
2024-09-08 18:13:11 +1000

    add task schema and model

---
+++ app/utils/tasks.server.ts
@@ -0,0 +1,51 @@
+import { prisma } from "./prisma.server";
+import { json } from "@remix-run/node";
+import { TaskData } from "../utils/types.server";
+
+export const getMyTasks = async (userID: string) => {
+  if (userID) {
+    const taskById = await prisma.user.findUnique({
+      where: {
+        id: userID,
+      },
+      include: {
+        tasks: {
+          orderBy: {
+            createdAt: "desc",
+          },
+        },
+      },
+    });
+    return taskById;
+  }
+
+  if (!userID) {
+    return json({ error: `The users doesnot have any tasks` });
+  }
+};
+
+export const createTask = async ({ category, message, taskId }: TaskData) => {
+  const taskById = await prisma.task.create({
+    data: { category, message, taskId },
+  });
+  if (!taskById) {
+    return json({ error: "Could not post the task" });
+  }
+  return json({
+    message: "Task created successfully",
+    success: "true",
+    payload: taskById,
+  });
+};
+
+export const deleteTask = async (id: string) => {
+  const taskById = await prisma.task.delete({ where: { id } });
+  if (!taskById) {
+    return json({ error: "Could not post the task" });
+  }
+  return json({
+    message: "Task deleted",
+    success: "true",
+    payload: id,
+  });
+};
